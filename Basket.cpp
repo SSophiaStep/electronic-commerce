ce1506545d960f4fde00552cde5748ce56168389
SSophiaStep <stepaniatova.sofiia@chnu.edu.ua>
2024-03-14 19:03:54 +0200

    Add has-a composition

--- Basket.cpp
+++ Basket.cpp
@@ -1,28 +1,21 @@
 #include "Basket.h"
-#include "Customer.h"
+#include "Person.h"
 #include <vector>
 #include <iostream>
 using namespace std;
 void Basket::AddItem(const string& item) {
     items.push_back(item);
 }
-
-void Basket::RemoveItem(const string& item) {
-    for (size_t i = 0; i < items.size(); ++i) {
-        if (items[i] == item) {
-            items.erase(items.begin() + i);
-            return;
-        }
-    }
-}
 void Basket::ShowItems() {
-    cout << "Items in the basket for " << getName() << ":" << endl;
+    cout<<"============================================================"<<endl;
+    cout << "Items in the basket for " << ":" << endl;
     for (const auto& item : items) {
         cout << "- " << item << endl;
+        cout<<"============================================================"<<endl;
     }
 }
 
-Basket::Basket(const string& customer_name, const string& customer_address, const string& customer_email)
-        : Customer(customer_name, customer_address, customer_email), items() {}
+Basket::Basket(const string& person_name, const string& person_address, const string& person_email)
+        : person(person_name, person_address, person_email), items() {}
 
 Basket::~Basket() {}//basket
--- Basket.h
+++ Basket.h
@@ -1,19 +1,19 @@
 #ifndef INC_2_BASKET_H
 #define INC_2_BASKET_H
 #include <iostream>
-#include "Customer.h"
+#include "Person.h"
 #include <vector>
 using namespace std;
 
-class Basket:public Customer{
+class Basket{
 private:
     vector<string> items;
+    Person person;
 public:
     void AddItem(const string& item);
-    void RemoveItem(const string& item);
     void ShowItems();
     Basket(const Basket& other);
-    Basket(const string& customer_name, const string& customer_address, const string& customer_email);
+    Basket(const string& person_name, const string& person_address, const string& person_email);
     ~Basket();
 };
 
